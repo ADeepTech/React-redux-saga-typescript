e2f5668affb9676cea55aaef9a4ab7208307b5a9
ADeepTech <56194333+ADeepTech@users.noreply.github.com>
2021-09-27 13:21:21 +0800

    Update Loadable.ts

--- src/utils/Loadable.ts
+++ src/utils/Loadable.ts
@@ -1,44 +1,46 @@
-export type Loadable<S> = ILoaded<S> | ILoading | IFailed | IReloading<S>;
+export type Loadable<S> = ILoaded<S> | ILoading | IFailed | IReloading<S>
 
 interface ILoaded<S> {
-  status: "LOADED";
-  data: S;
+    status: "LOADED"
+    data: S
 }
 
 interface ILoading {
-  status: "LOADING";
+    status: "LOADING"
 }
 
 interface IReloading<S> {
-  status: "RELOADING";
-  data: S;
+    status: "RELOADING"
+    data: S
 }
 
 interface IFailed {
-  status: "FAILED";
-  error: Error | string | undefined | null;
-  message: string | undefined | null;
+    status: "FAILED"
+    error: Error | string | undefined | null
+    message: string | undefined | null
 }
 
 export const Loading = <S>(): Loadable<S> => ({
-  status: "LOADING"
-});
+    status: "LOADING",
+})
 export const Reloading = <S>(s: S): Loadable<S> => ({
-  status: "RELOADING",
-  data: s
-});
+    status: "RELOADING",
+    data: s,
+})
 export const Loaded = <S>(s: S): Loadable<S> => ({
-  status: "LOADED",
-  data: s
-});
+    status: "LOADED",
+    data: s,
+})
 export const Failed = <S>(e: Error | string | undefined | null, message?: string | undefined | null): Loadable<S> => ({
-  status: "FAILED",
-  error: e,
-  message
-});
+    status: "FAILED",
+    error: e,
+    message,
+})
 
-export const isLoaded = <S>(s: Loadable<S> | undefined | null): s is ILoaded<S> => s != null && s.status === "LOADED";
-export const isLoading = <S>(s?: Loadable<S> | undefined | null): s is ILoading => s == null || s.status === "LOADING";
-export const isReloading = <S>(s?: Loadable<S> | undefined | null): s is IReloading<S> =>
-  s != null && s.status === "RELOADING";
-export const isFailed = <S>(s: Loadable<S> | undefined | null): s is IFailed => s != null && s.status === "FAILED";
+type LoadableParam<S> = Loadable<S> | undefined | null
+
+export const isLoaded = <S>(s: LoadableParam<S>): s is ILoaded<S> => s != null && s.status === "LOADED"
+export const isLoading = <S>(s?: LoadableParam<S>): s is ILoading => s == null || s.status === "LOADING"
+export const isReloading = <S>(s?: LoadableParam<S>): s is IReloading<S> =>
+    s != null && s.status === "RELOADING"
+export const isFailed = <S>(s: LoadableParam<S>): s is IFailed => s != null && s.status === "FAILED"
